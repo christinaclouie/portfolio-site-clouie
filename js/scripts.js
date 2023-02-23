d5c1145641c16c3532e6beceaca4d9ba95411e40
Christina Louie <christinaclouie@gmail.com>
2023-02-22 19:04:24 -0600

    Updated JS, CSS, and HTML Files
    -Referenced the CDN into the index file
    -Added hamburger menu icon to the top nav for mobile screen
    -Updated CSS file to format the hamburger menu icon
    -Updated JS file to add functionality to the hamburger menu icon

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("Hi! Welcome to my portfolio!")
+//menu toggle for web responsive sizes
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+    } else {
+        x.classname = 'navtoggle';
+    }
+}
